560c6072f3f051a8ee40f09289af22e65db48068
Konrad Talik <konradtalik@gmail.com>
2018-06-18 22:21:28 +0200

    Update setup.py regarding countries

--- setup.py
+++ setup.py
@@ -8,15 +8,15 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django-language-flags',
+    name='django-country-and-language-flags',
     version='0.1.1',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     include_package_data=True,
     license='BSD License',
-    description='A simple Django app to display national flags for languages.',
+    description='Display country flags or sets of flags of nations where a particular language is spoken',
     long_description=README,
-    url='https://github.com/anselmlingnau/django-language-flags',
+    url='https://github.com/ktalik/django-country-and-language-flags',
     author='Anselm Lingnau',
     author_email='anselm@anselms.net',
     classifiers=[
